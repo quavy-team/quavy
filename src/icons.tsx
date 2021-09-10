431de0ff1c0b29d03bc3585fca657fcb2170bcbf
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2021-09-09 21:57:22 -0300

    update

--- src/icons.tsx
+++ src/icons.tsx
@@ -12,8 +12,8 @@ export const Menu = () => (
       initial={{ pathLength: 0, opacity: 0 }}
       animate={{ pathLength: 1, opacity: 1 }}
       transition={{
-        duration: 3,
-        delay: 1,
+        duration: 1,
+        delay: 0.2,
         ease: "easeOut",
       }}
       strokeLinecap="round"
@@ -36,8 +36,8 @@ export const Home = () => (
       initial={{ pathLength: 0, opacity: 0 }}
       animate={{ pathLength: 1, opacity: 1 }}
       transition={{
-        duration: 3,
-        delay: 1,
+        duration: 1,
+        delay: 0.4,
         ease: "easeOut",
       }}
       strokeLinecap="round"
@@ -60,14 +60,62 @@ export const Search = () => (
       initial={{ pathLength: 0, opacity: 0 }}
       animate={{ pathLength: 1, opacity: 1 }}
       transition={{
-        duration: 3,
-        delay: 1,
+        duration: 1,
+        delay: 0.6,
         ease: "easeOut",
       }}
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2}
       d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+    />
+  </motion.svg>
+);
+
+export const Chord = () => (
+  <motion.svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <motion.path
+      initial={{ pathLength: 0, opacity: 0 }}
+      animate={{ pathLength: 1, opacity: 1 }}
+      transition={{
+        duration: 1,
+        delay: 0.8,
+        ease: "easeOut",
+      }}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+    />
+  </motion.svg>
+);
+
+export const LogIn = () => (
+  <motion.svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <motion.path
+      initial={{ pathLength: 0, opacity: 0 }}
+      animate={{ pathLength: 1, opacity: 1 }}
+      transition={{
+        duration: 1,
+        delay: 1,
+        ease: "easeOut",
+      }}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
     />
   </motion.svg>
 );
@@ -84,7 +132,7 @@ export const Info = () => (
       initial={{ pathLength: 0, opacity: 0 }}
       animate={{ pathLength: 1, opacity: 1 }}
       transition={{
-        duration: 3,
+        duration: 1,
         delay: 1,
         ease: "easeOut",
       }}
@@ -95,6 +143,31 @@ export const Info = () => (
     />
   </motion.svg>
 );
+
+export const Close = () => (
+  <motion.svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <motion.path
+      initial={{ pathLength: 0 }}
+      animate={{ pathLength: 1 }}
+      transition={{
+        duration: 1,
+        delay: 0,
+        ease: "easeOut",
+      }}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M6 18L18 6M6 6l12 12"
+    />
+  </motion.svg>
+);
+
 export const User = () => (
   <motion.svg
     xmlns="http://www.w3.org/2000/svg"
@@ -107,7 +180,7 @@ export const User = () => (
       initial={{ pathLength: 0, opacity: 0 }}
       animate={{ pathLength: 1, opacity: 1 }}
       transition={{
-        duration: 3,
+        duration: 1,
         delay: 1,
         ease: "easeOut",
       }}
@@ -115,6 +188,43 @@ export const User = () => (
       strokeLinejoin="round"
       strokeWidth={2}
       d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </motion.svg>
+);
+
+export const Duo = () => (
+  <motion.svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <motion.path
+      initial={{ pathLength: 0, opacity: 0 }}
+      animate={{ pathLength: 1, opacity: 1 }}
+      transition={{
+        duration: 1,
+        delay: 1,
+        ease: "easeOut",
+      }}
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M12.7929 12.7929C13.1834 12.4024 13.8166 12.4024 14.2071 12.7929L20.7071 19.2929C21.0976 19.6834 21.0976 20.3166 20.7071 20.7071C20.3166 21.0976 19.6834 21.0976 19.2929 20.7071L12.7929 14.2071C12.4024 13.8166 12.4024 13.1834 12.7929 12.7929Z"
+      fill="#333"
+      // fillOpacity="0.25"
+    />
+    <motion.path
+      initial={{ pathLength: 0, opacity: 0 }}
+      animate={{ pathLength: 1, opacity: 1 }}
+      transition={{
+        duration: 1,
+        delay: 1,
+        ease: "easeOut",
+      }}
+      d="M10 3C6.13401 3 3 6.13401 3 10C3 13.866 6.13401 17 10 17C13.866 17 17 13.866 17 10C17 6.13401 13.866 3 10 3Z"
+      // fill="#12131A"
+      fill="#5575fe"
     />
   </motion.svg>
 );
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,7 @@
-import * as outline from "@heroicons/react/outline";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import React, * as react from "react";
-import icons from "src/icons"
+import * as react from "react";
+import icons from "src/icons";
 
 type Props = {
   children: react.ReactNode;
@@ -18,38 +17,59 @@ export default function Layout({ children, className, center }: Props) {
   );
 
   function NavBar() {
+    const [menuOpened, setMenuOpened] = react.useState(false);
     return (
-      // backgroundColor: "var(--light)"
-      <motion.nav>
-        {/* <motion.button onClick={() => {
-        }}></motion.button> */}
-        <NavLink href="#" Icon={icons.Menu}>
-          Menu
-        </NavLink>
-        <NavLink href="#" Icon={icons.Home}>
-          Home
-        </NavLink>
-        <NavLink href="/b/" Icon={icons.Search}>
-          Buscar
-        </NavLink>
-        <NavLink href="/i/" Icon={icons.Info}>
-          About
-        </NavLink>
-        <NavLink href="/c/" Icon={icons.User}>
-          Cuenta
-        </NavLink>
+      <motion.nav
+        custom={menuOpened}
+        animate={{ width: menuOpened ? 300 : 60 }}
+        transition={{ when: "beforeChildren" }}
+      >
+        <MenuBtn
+          Icon={menuOpened ? icons.Close : icons.Menu}
+          onClick={() => setMenuOpened(!menuOpened)}
+        />
+        {menuOpened ? (
+          <>
+            <MenuBtn Icon={icons.Home}>Cambiar color</MenuBtn>
+            <hr></hr>
+            <motion.h2
+              initial={{ opacity: 0 }}
+              animate={{opacity: 1}}
+            >Songly</motion.h2>
+          </>
+        ) : (
+          <>
+            <NavLink href="/" Icon={icons.Home}></NavLink>
+            <NavLink href="/b/" Icon={icons.Search}></NavLink>
+            {/* <NavLink href="/b/" Icon={icons.Duo}></NavLink> */}
+            <NavLink href="/a/" Icon={icons.Chord}></NavLink>
+            <NavLink href="/c/" Icon={icons.LogIn}></NavLink>
+          </>
+        )}
       </motion.nav>
     );
   }
 
-  function NavLink({ href, Icon, children }: any) {
+  function NavLink({ href, Icon }: any) {
     return (
-      <Link href={href}>
-        <a>
+      <Link href={href} passHref>
+        <motion.a>
           <Icon />
-          <p>{children}</p>
-        </a>
+        </motion.a>
       </Link>
+    );
+  }
+
+  function MenuBtn({ children, Icon, ...props }: any) {
+    return (
+      <motion.button
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        {...props}
+      >
+        <Icon />
+        {children}
+      </motion.button>
     );
   }
 }
