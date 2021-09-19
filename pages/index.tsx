47520cd7fa380c51c75df58fd6effed5c6d168f1
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2021-09-19 11:51:46 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,16 @@
 import { motion } from "framer-motion";
 import type { NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "src/layout";
 import styles from "styles/Home.module.sass";
 
 const Home: NextPage = () => {
+  const verification = "ZZ5PyjP_GYDCeTkuTebsWOhQgdoiWH2XbJYpnxFueIk";
   return (
     <Layout className={styles.container}>
       <Head>
         <title>Welcome to Quavy App</title>
+        <meta name="google-site-verification" content={verification} />
       </Head>
       <motion.h1 className={styles.title}>Welcome to Quavy App</motion.h1>
     </Layout>
