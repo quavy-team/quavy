f29ea4b475667ba311c704c648947b65ae44a07b
Joaquin Martinez <54916596+Joaquinuriel@users.noreply.github.com>
2022-01-12 18:49:04 -0300

    db-update-fixed

--- pages/cuenta.tsx
+++ pages/cuenta.tsx
@@ -1,26 +1,10 @@
 import * as next from "@nextui-org/react";
 import Layout from "src/layout";
 
-var string = "primary";
-var number = 15000;
-var fn = (pmeter) => pmeter;
-JSON.parse(string);
-fn(number);
-
-export default function Cuenta(props) {
-  const something = {
-    name: "joaquin",
-    age: 17,
-    dev: true,
-  };
-  console.log(something, props.something);
-  var string = "primary";
-  JSON.parse(string);
+export default function Cuenta() {
   return (
     <Layout>
-      <next.Text h1 color={string}>
-        Quavy Cuenta
-      </next.Text>
+      <next.Text h1>Quavy Cuenta</next.Text>
     </Layout>
   );
 }
